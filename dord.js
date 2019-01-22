89cb2722d004939ec54091390b98f3de835fd639
jjkaufman <jonathanjkaufman@gmail.com>
2019-01-21 22:34:58 -0800

    reorg + water

--- dord.js
+++ dord.js
@@ -1,5 +1,5 @@
 function insertCanvi() {
-    var types = ['vanilla', 'shrink', 'grow', 'explode', 'curve', 'spin', 'art', 'freedom', 'punch', 'wiggle', 'surprise', 'slam', 'squeeze', 'pulse', 'goodbye', 'cross', 'luck', 'ding', 'fill', 'spill', 'subwoofer', 'spotlight', 'fire'];
+    var types = ['water', 'vanilla', 'shrink', 'grow', 'explode', 'curve', 'spin', 'art', 'freedom', 'punch', 'wiggle', 'surprise', 'slam', 'squeeze', 'pulse', 'goodbye', 'cross', 'luck', 'ding', 'fill', 'spill', 'subwoofer', 'spotlight', 'fire'];
 
     var dords = Array.from(document.getElementsByClassName('dord'))
         // add the custom html tag versions
@@ -19,16 +19,23 @@ function insertCanvi() {
         let y = dord.getBoundingClientRect().top;
         var canvas = document.createElement('canvas');
         canvas.width = width * 2;
+
+        dordStyle = window.getComputedStyle(dord);
+
+
+        canvas.style.verticalAlign = 'bottom';
+
+
         canvas.style.width = width + 'px';
         canvas.height = height * 2;
         canvas.style.height = height + 'px';
         canvas.classList.add('dord-canvas');
-        canvas.style.verticalAlign = 'bottom';
         canvas.dataset.text = dord.textContent;
-        canvas.dataset.fontSize = window.getComputedStyle(dord).fontSize;
-        canvas.dataset.fontFamily = window.getComputedStyle(dord).fontFamily;
-        canvas.dataset.color = window.getComputedStyle(dord).color;
+        canvas.dataset.fontSize = dordStyle.fontSize;
+        canvas.dataset.fontFamily = dordStyle.fontFamily;
+        canvas.dataset.color = dordStyle.color;
         canvas.dataset.type = dord.dataset.dordType;
+
         dord.parentNode.insertBefore(canvas, dord);
         dord.style.display = 'none';
     })
@@ -109,6 +116,9 @@ function renderCanvi() {
         }
         if (canvas.dataset.type == 'fire') {
             renderFire(canvas);
+        }
+        if (canvas.dataset.type == 'water') {
+            renderWater(canvas);
         }
 
     }
@@ -207,6 +217,8 @@ function renderSpin(canvas) {
 }
 function renderArt(canvas) {
     var ctx = canvas.getContext('2d');
+    var size = canvas.dataset.fontSize;
+    size = size.substring(0, size.length - 2);
 
     var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
     gradient.addColorStop("0", "#BA0D8D");
@@ -216,10 +228,10 @@ function renderArt(canvas) {
     gradient.addColorStop(4 / 5, "#1944A0");
     gradient.addColorStop(5 / 5, "#4E25A9");
     ctx.fillStyle = gradient;
-    ctx.shadowBlur = 5;
+    ctx.shadowBlur = size / 9.6;
     ctx.shadowColor = '#C2C2C2';
-    ctx.shadowOffsetX = '-5';
-    ctx.shadowOffsetY = '-10';
+    ctx.shadowOffsetX = -1 * size / 9.6;
+    ctx.shadowOffsetY = -2 * size / 9.6;
 
     var size = canvas.dataset.fontSize;
     size = size.substring(0, size.length - 2);
@@ -231,7 +243,7 @@ function renderFreedom(canvas) {
     var size = canvas.dataset.fontSize;
     size = size.substring(0, size.length - 2);
     ctx.font = size * 2 + "px " + canvas.dataset.fontFamily;
-    ctx.fillText(canvas.dataset.text, 5, size * 1.75 + 5);
+    ctx.fillText(canvas.dataset.text, size / 9.6, size * 1.75 + (size / 9.6));
 
     var gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
     gradient.addColorStop("0", "#99B1CD");
@@ -322,6 +334,9 @@ function renderSurprise(canvas, frame = 0) {
 
     window.requestAnimationFrame(function () {
         frame++
+        if (frame > canvas.width) {
+            frame = 0
+        }
         renderSurprise(canvas, frame);
     });
 
@@ -353,6 +368,9 @@ function renderSlam(canvas, frame = 0) {
 
     window.requestAnimationFrame(function () {
         frame++
+        if (frame > canvas.width) {
+            frame = 0
+        }
         renderSlam(canvas, frame);
     });
 
@@ -606,7 +624,7 @@ function renderFill(canvas, frame = 0) {
     ctx.strokeText(canvas.dataset.text, 0, size * 1.75);
 
     window.requestAnimationFrame(function () {
-        frame += canvas.width / 2000;
+        frame += canvas.height / 150;
         if (frame > canvas.height) {
             frame = 0;
         }
@@ -715,7 +733,7 @@ function renderFire(canvas) {
     var ctx = canvas.getContext('2d');
     var size = canvas.dataset.fontSize;
     size = size.substring(0, size.length - 2);
-    var text = canvas.dataset.text ;
+    var text = canvas.dataset.text;
     ctx.font = size * 2 + "px " + canvas.dataset.fontFamily;
     ctx.fillText(text, 0, size * 1.75);
     drawFire(canvas);
@@ -724,121 +742,156 @@ function renderFire(canvas) {
 function drawFire(canvas) {
     const firePixelsArray = [];
     var ctx = canvas.getContext('2d');
-    const originalPixelsArray = ctx.getImageData(0,0,canvas.width,canvas.height).data;
-    
+    const originalPixelsArray = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+
     let fireWidth = canvas.width
     let fireHeight = canvas.height
-    const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
+    const fireColorsPalette = [{ "r": 7, "g": 7, "b": 7 }, { "r": 31, "g": 7, "b": 7 }, { "r": 47, "g": 15, "b": 7 }, { "r": 71, "g": 15, "b": 7 }, { "r": 87, "g": 23, "b": 7 }, { "r": 103, "g": 31, "b": 7 }, { "r": 119, "g": 31, "b": 7 }, { "r": 143, "g": 39, "b": 7 }, { "r": 159, "g": 47, "b": 7 }, { "r": 175, "g": 63, "b": 7 }, { "r": 191, "g": 71, "b": 7 }, { "r": 199, "g": 71, "b": 7 }, { "r": 223, "g": 79, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 103, "b": 15 }, { "r": 207, "g": 111, "b": 15 }, { "r": 207, "g": 119, "b": 15 }, { "r": 207, "g": 127, "b": 15 }, { "r": 207, "g": 135, "b": 23 }, { "r": 199, "g": 135, "b": 23 }, { "r": 199, "g": 143, "b": 23 }, { "r": 199, "g": 151, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 175, "b": 47 }, { "r": 183, "g": 175, "b": 47 }, { "r": 183, "g": 183, "b": 47 }, { "r": 183, "g": 183, "b": 55 }, { "r": 207, "g": 207, "b": 111 }, { "r": 223, "g": 223, "b": 159 }, { "r": 239, "g": 239, "b": 199 }, { "r": 255, "g": 255, "b": 255 }]
 
     const context = canvas.getContext('2d')
     const image = context.createImageData(fireWidth, fireHeight)
-    
+
     function start() {
-      createFireDataStructure()
-      setInterval(createFireSource, 50);
-      setInterval(calculateFirePropagation, 50)
+        createFireDataStructure()
+        setInterval(createFireSource, 50);
+        setInterval(calculateFirePropagation, 50)
     }
-    
+
     function createFireDataStructure() {
-      const numberOfPixels = fireWidth * fireHeight
-    
-      for (let i = 0; i < numberOfPixels; i++) {
-        firePixelsArray[i] = 0
-      }
-    }
-    
-    function calculateFirePropagation() {
-      for (let column = 0; column < fireWidth; column++) {
-        for (let row = 0; row < fireHeight; row++) {
-          const pixelIndex = column + ( fireWidth * row )
-    
-          updateFireIntensityPerPixel(pixelIndex)
+        const numberOfPixels = fireWidth * fireHeight
+
+        for (let i = 0; i < numberOfPixels; i++) {
+            firePixelsArray[i] = 0
         }
-      }
-    
-      renderFire()
+    }
+
+    function calculateFirePropagation() {
+        for (let column = 0; column < fireWidth; column++) {
+            for (let row = 0; row < fireHeight; row++) {
+                const pixelIndex = column + (fireWidth * row)
+
+                updateFireIntensityPerPixel(pixelIndex)
+            }
+        }
+
+        renderFire()
 
     }
-    
+
     function updateFireIntensityPerPixel(currentPixelIndex) {
-      const belowPixelIndex = currentPixelIndex + fireWidth
-    
-      // below pixel index overflows canvas
-      if (belowPixelIndex >= fireWidth * fireHeight) {
-        return
-      }
-    
-      const decay = Math.floor(Math.random() * 3)
-      const belowPixelFireIntensity = firePixelsArray[belowPixelIndex]
-      const newFireIntensity =
-        belowPixelFireIntensity - decay >= 0 ? belowPixelFireIntensity - decay : 0
-      firePixelsArray[currentPixelIndex - decay] = newFireIntensity
+        const belowPixelIndex = currentPixelIndex + fireWidth
+
+        // below pixel index overflows canvas
+        if (belowPixelIndex >= fireWidth * fireHeight) {
+            return
+        }
+
+        const decay = Math.floor(Math.random() * 3)
+        const belowPixelFireIntensity = firePixelsArray[belowPixelIndex]
+        const newFireIntensity =
+            belowPixelFireIntensity - decay >= 0 ? belowPixelFireIntensity - decay : 0
+        firePixelsArray[currentPixelIndex - decay] = newFireIntensity
     }
-    
+
     function renderFire() {
-      for (let pixelIndex = 0; pixelIndex < firePixelsArray.length; pixelIndex++) {
-        const fireIntensity = firePixelsArray[pixelIndex]
-        const color = fireColorsPalette[fireIntensity]
+        for (let pixelIndex = 0; pixelIndex < firePixelsArray.length; pixelIndex++) {
+            const fireIntensity = firePixelsArray[pixelIndex]
+            const color = fireColorsPalette[fireIntensity]
             image.data[pixelIndex * 4] = color.r
             image.data[pixelIndex * 4 + 1] = color.g
-        
+
             image.data[pixelIndex * 4 + 2] = color.b
-            image.data[pixelIndex * 4 + 3] =  fireIntensity < 10 ? 0 : 255;
-      }
-      context.putImageData(image, 0, 0)
+            image.data[pixelIndex * 4 + 3] = fireIntensity < 10 ? 0 : 255;
+        }
+        context.putImageData(image, 0, 0)
     }
-    
+
     function createFireSource() {
-        for(var o = 0; o < originalPixelsArray.length; o+=4){
-            if(originalPixelsArray[o + 3] != 0){
+        for (var o = 0; o < originalPixelsArray.length; o += 4) {
+            if (originalPixelsArray[o + 3] != 0) {
                 firePixelsArray[o / 4] = 30
             }
         }
     }
-    
-    function destroyFireSource() {
-      for (let column = 0; column <= fireWidth; column++) {
-        const overflowPixelIndex = fireWidth * fireHeight
-        const pixelIndex = (overflowPixelIndex - fireWidth) + column
-    
-        firePixelsArray[pixelIndex] = 0
-      }
-    }
-    
-    function increaseFireSource(newFireIntensity = null) {
-      for (let column = 0; column <= fireWidth; column++) {
-        const overflowPixelIndex = fireWidth * fireHeight
-        const pixelIndex = (overflowPixelIndex - fireWidth) + column
-        const currentFireIntensity = firePixelsArray[pixelIndex]
-    
-        if (currentFireIntensity < 36) {
-            if(newFireIntensity == null){
-                const increase = Math.floor(Math.random() * 14)
-                newFireIntensity =
-                currentFireIntensity + increase >= 36 ? 36 : currentFireIntensity + increase
-            }
-          firePixelsArray[pixelIndex] = newFireIntensity
-        }
-      }
-    }
-    
-    function decreaseFireSource() {
-      for (let column = 0; column <= fireWidth; column++) {
-        const overflowPixelIndex = fireWidth * fireHeight
-        const pixelIndex = (overflowPixelIndex - fireWidth) + column
-        const currentFireIntensity = firePixelsArray[pixelIndex]
-    
-        if (currentFireIntensity > 0) {
-          const decay = Math.floor(Math.random() * 14)
-          const newFireIntensity =
-            currentFireIntensity - decay >= 0 ? currentFireIntensity - decay : 0
-    
-          firePixelsArray[pixelIndex] = newFireIntensity
-        }
-      }
-    }
-    
+
     start()
 }
+
+//inspired by https://ariya.io/2012/03/underwater-effect-with-html5-canvas
+function renderWater(canvas) {
+    var ctx = canvas.getContext('2d');
+
+    var size = canvas.dataset.fontSize;
+    size = size.substring(0, size.length - 2);
+    ctx.font = size * 2 + "px " + canvas.dataset.fontFamily;
+    ctx.fillText(canvas.dataset.text, 0, size * 1.75);
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    console.log(size);
+    var fx = Underwater(canvas, size / 16 * 3, .6 * 10);
+    fx.start();
+
+    //edits made to original from https://github.com/fffaraz/Etcetera
+    function Underwater(canvas, amplitude, frequency) {
+        var context = canvas.getContext('2d'),
+            width = canvas.width,
+            stride = width * 4,
+            height = canvas.height,
+            result = context.createImageData(width, height),
+            pixels = new Array(4 * width * height),
+            interval = 1000 / 60,
+            frames = 0;
+
+        function init() {
+            var i, source;
+            source = context.getImageData(0, 0, width, height);
+            result = context.createImageData(width, height);
+            for (i = 0; i < 4 * height * width; i += 1) {
+                pixels[i] = source.data[i];
+                result.data[i] = 0;
+            }
+        }
+
+        function apply() {
+            var r, T, x, y, xs, ys, dest, src;
+
+            r = result.data;
+
+            T = frames * interval * frequency / 1000;
+            for (x = amplitude; x < width - amplitude; ++x) {
+                for (y = amplitude; y < height - amplitude; ++y) {
+                    xs = amplitude * Math.sin(2 * Math.PI * (3 * y / height + T));
+                    ys = amplitude * Math.cos(2 * Math.PI * (3 * x / width + T));
+                    xs = Math.round(xs);
+                    ys = Math.round(ys);
+                    dest = y * stride + x * 4;
+                    src = (y + ys) * stride + (x + xs) * 4;
+                    r[dest] = pixels[src];
+                    r[dest + 1] = pixels[src + 1];
+                    r[dest + 2] = pixels[src + 2];
+                    r[dest + 3] = pixels[src + 3];
+                }
+            }
+
+            context.putImageData(result, 0, 0);
+            frames += 1;
+
+            // Force flushing the pending painting.
+            context.getImageData(0, 0, 1, 1);
+
+            window.requestAnimationFrame(apply);
+        }
+
+        function start() {
+            window.requestAnimationFrame(apply);
+        }
+
+        init();
+
+        return {
+            start: start
+        };
+    }
+}
+
 insertCanvi();
 renderCanvi();
